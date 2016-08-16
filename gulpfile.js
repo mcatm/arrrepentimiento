8016b6d78bcde03829f086fc3dbba315b53887fe
mcatm <tkcs@pelepop.com>
2016-08-16 14:53:51 +0900

    add gulpfiles

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp          = require("gulp"),
+    autoprefixer  = require("gulp-autoprefixer"),
+    plumber       = require("gulp-plumber"),
+    concat        = require('gulp-concat'),
+    bulkSass      = require('gulp-sass-bulk-import'),
+    sass          = require("gulp-sass");
+
+gulp.task("sass", function() {
+  gulp.src("src/scss/**/style.scss")
+      .pipe(plumber())
+      .pipe(bulkSass())
+      .pipe(sass( {outputStyle: 'compressed'} ))
+      .pipe(autoprefixer())
+      .pipe(gulp.dest("./www/html/css"));
+});
+
+gulp.task("default", function() {
+  gulp.watch("sass/**/*.scss",["sass"]);
+});
