90fdffb582145653f7e452b5178fa602f24af7f1
mcatm <tkcs@pelepop.com>
2020-11-16 09:56:55 +0900

    update

--- pages/about.vue
+++ pages/about.vue
@@ -15,7 +15,7 @@
     </div>
 
     <div class="l-mainimg">
-      <img src="~assets/images/about01.jpg" />
+      <img src="~assets/images/collective/001.jpg" />
     </div>
 
     <div class="l-main">
@@ -25,28 +25,33 @@
             <em>In the burnt red-black sky, the gospel resonates in capital HELVETICA.</em><br>
             焦げた赤黒い空に、大文字のヘルベチカで鳴り響く福音。
           </p>
-
           <p>
             <em>At the beach where the sadness ended, preparations for a new party begin.</em><br>
             悲しみの終わった浜にて、新しい宴の準備が始まる。
           </p>
-
           <p>
             <em>Buildings of sound constructed in fictitious dimensions are now irradiated to the body from completely different angles.</em><br>
             架空の次元で構築された音の建造物が、今まさに全く別の角度から肉体に照射された。
           </p>
-          
           <p>
             <em>A melody is born like breathing, and rhythm dies as if to breathe out.</em><br>
             息を吸うようにメロディが生まれ、息を吐くようにリズムが死んでいく。
           </p>
-
           <p>
             <em>Let's say goodbye to old-fashioned magic. At the entrance of the mellow cavity era.</em><br>
             古ぼけた魔術にさよならを告げよう。芳醇な空洞時代の入り口にて。
           </p>
         </div>
+      </div>
 
+      <div class="l-columns reverse">
+        <div class="column">
+          <p class="en"><strong>We build the modern folk songs by home-recording. The miscellaneous members give them special bodies by the experimental methods that ignore any contexts and add strange atmosphere. Between confusion and silence.</strong></p>
+          <p class="ja"><small>実験音楽とモダンフォークの光輝なる融合。知る人ぞ知るエディットサイケバンドdrawing4-5を母体とするコレクティブ「Arrrepentimiento（アレペンティミエント）」。日本語で「後悔」。雑多なメンバーが入れ替わり立ち替わり参加したり離れたりしながら、コンテキストを無視した演奏と奇怪な音像で、断片的なメロディに実体を与えていく事故音楽。ノイズ〜コラージュといった実験音楽と、リリカルなフォークソングの厳かな融合が、一瞬の儀式のように駆け抜けていきます。</small></p>
+        </div>
+        <div class="column">
+          <p class="img"><img src="~assets/images/collages/002.jpg" /></p>
+        </div>
       </div>
 
 
@@ -154,6 +159,47 @@ export default {
     }
   }
 
+}
+
+.l-columns {
+  padding: 0 0 80px;
+  .column {
+    width: 50%;
+    padding: 0 40px;
+    &:first-child { padding-left: 0;}
+    &:last-child { padding-right: 0;}
+    @include sp {
+      width: 100%;
+      padding: 0 0 30px;
+    }
+    p {
+      margin: 0;
+      padding: 0 0 20px;
+      &.en {
+        line-height: 1.8;
+        font-size: 1.1em;
+        text-align: justify;
+      }
+      &.ja {
+        line-height: 1.6;
+      }
+    }
+    .img {
+      > img {
+        max-width: 100%;
+        max-height: 100%;
+        width: auto;
+        height: auto;
+      }
+      @include sp {
+        max-height: 300px;
+        overflow: hidden;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    }
+  }
 }
 
 .heading {
