d217d6e703cc5ccb7099bbe8720e8156e94784d6
mcatm <tkcs@pelepop.com>
2020-11-30 16:56:45 +0900

    update links

--- plugins/links.ts
+++ plugins/links.ts
@@ -8,7 +8,7 @@ export const internalLinks = [
 export const externalLinks = [
   { url: 'https://arrrepentimiento.bandcamp.com/', label: 'Bandcamp' },
   { url: 'https://www.youtube.com/channel/UCBJTDTjztVTlISvCE1HEkMw/', label: 'YouTube' },
-  { url: 'https://open.spotify.com/artist/7v2cd2Ai9FvjIAxVkbcIiL', label: 'Spotify' },
+  { url: 'https://open.spotify.com/artist/7AtZYrBxI7ZBvLx5pd357m', label: 'Spotify' },
   { url: 'https://music.apple.com/jp/artist/arrrepentimiento/1482194663', label: 'iTunes' },
   { url: 'https://soundcloud.com/arrrepentimiento', label: 'SoundCloud' },
   { url: 'https://twitter.com/arrrrps', label: 'Twitter' },
